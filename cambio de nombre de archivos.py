c381ff86e1e5853fa1efc943987380f4fe3dc144
Nahuel Vargas <68164200+nvargas24@users.noreply.github.com>
2025-04-21 07:58:16 -0300

    py de manejo de archivos

---
+++ cambio de nombre de archivos.py
@@ -0,0 +1,24 @@
+import os
+import re
+
+# Ruta de la carpeta
+carpeta = r"T:\MaterialRodante\Laboratorio\Laboratorio de Electrónica\Reparaciones\Notas de Reparación"
+
+
+# Recorrer todos los archivos en la carpeta
+for archivo in os.listdir(carpeta): 
+    if archivo.endswith('.doc') or archivo.endswith('.docx'):
+        # Si el archivo comienza con 'Nota'
+        if archivo.startswith('Nota'):
+            # Crear nuevo nombre con el prefijo añadido
+            nuevo_nombre = 'LS-MR-CT-R-000 ' + archivo
+            # Obtener la ruta completa de los archivos
+            ruta_vieja = os.path.join(carpeta, archivo)
+            ruta_nueva = os.path.join(carpeta, nuevo_nombre)
+            # Renombrar el archivo
+            os.rename(ruta_vieja, ruta_nueva)
+            print(f'Renombrado: {ruta_vieja} a {ruta_nueva}')
+        else:
+            print(f'Sin cambios: {archivo}')
+    else:
+        print(f'Archivo no considerado: {archivo}')
