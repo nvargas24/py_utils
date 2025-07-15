e22b19b69d7c52cbc625b83899627a172c8f5063
NEVargas <nahuvargas24@gmail.com>
2025-07-15 16:06:30 -0300

    organizo data de archivo para cargar en xlsx

--- pdf/analize_files.py
+++ pdf/analize_files.py
@@ -1,4 +1,6 @@
 import os
+import win32security
+
 import datetime
 import pandas as pd
 
@@ -30,19 +32,39 @@ def update(df, extension):
                 sheet_name=type_files[extension]['name_sheet']
     )
 
-def register(root, name, extension):
+def extract_author(ruta):
+    # Obtener descriptor de seguridad del archivo
+    sd = win32security.GetFileSecurity(ruta, win32security.OWNER_SECURITY_INFORMATION)
+    # Extraer el SID del owner
+    owner_sid = sd.GetSecurityDescriptorOwner()
+    # Obtener nombre legible del owner
+    nombre_usuario, dominio, _ = win32security.LookupAccountSid(None, owner_sid)
+
+    return nombre_usuario
+
+def extract_data_file(root, name, extension):
+    dict_data_file = {}
+
     if name.lower().endswith(extension):
         ruta = os.path.join(root, name)
         try:
+            user_author = extract_author(ruta)
             fecha_mod = os.path.getmtime(ruta)
-            fecha_legible = datetime.datetime.fromtimestamp(fecha_mod)
-            
-            return fecha_legible, ruta
+            fecha_create = os.path.getctime(ruta)
+
+            dict_data_file['Nombre de archivo'] = name
+            dict_data_file['Fecha de creación'] = datetime.datetime.fromtimestamp(fecha_mod)
+            dict_data_file['Fecha ultima actualizacion'] = datetime.datetime.fromtimestamp(fecha_create)
+            dict_data_file['Autor'] = user_author
+            dict_data_file['Url'] = f'=HYPERLINK("{ruta}", "Abrir archivo")'
+
+            return dict_data_file
         
         except Exception as e:
             print(f"Error con el archivo: {ruta}")
-            return None, None
-    return None, None
+            return None
+    return None
+
 """
 OBS.:
 root: la ruta actual del directorio que se está recorriendo.
@@ -53,18 +75,13 @@ registros = []
 
 for root, dirs, files in os.walk(URL):
     for name in files: # obtiene nombre de archivos disponibles
-        last_date, url = register(root, name, type_files['word']['extension'])
-        if last_date and url:
-            registros.append({
-                "archivo": name,
-                "ultima_modificacion": last_date,
-                "ruta": url,
-            })
+        data_file = extract_data_file(root, name, type_files['word']['extension'])
+        if data_file:
+            registros.append(data_file)
 
 df = pd.DataFrame(registros)
-df['ruta'] = df['ruta'].apply(lambda x: f'=HYPERLINK("{x}", "Abrir archivo")')
 
 print(df)
-#print(df.info())
+print(df.info())
 
 update(df, 'word')
